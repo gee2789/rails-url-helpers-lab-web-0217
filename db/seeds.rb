e7dff90b822e82ac4e3a281388c33de25a14bce6
gee2789 <geeyung.li@gmail.com>
2017-03-01 09:37:53 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
+
+student = Student.create(first_name: "Gee", last_name: "Li")
+student = Student.create(first_name: "Hannah", last_name: "Banana")
+student = Student.create(first_name: "John", last_name: "Wick")
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
